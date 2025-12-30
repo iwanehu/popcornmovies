e78e3fc752ec9a033a12b404cca50f2156751089
iwanehu <120640856+iwanehu@users.noreply.github.com>
2025-12-30 23:57:24 +0100

    Refactor useEffect and fetchMovies in useMovies

--- src/useMovies.js
+++ src/useMovies.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+
 const KEY = "e8668079";
 
 export function useMovies(query) {
@@ -6,53 +7,47 @@ export function useMovies(query) {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
 
-  useEffect(
-    function () {
-      // callback?.();
+  useEffect(() => {
+    const controller = new AbortController();
 
-      const controller = new AbortController();
-
-      async function fetchMovies() {
-        try {
-          setIsLoading(true);
-          setError("");
-
-          
-
-        const res = await fetch( `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`, { signal: controller.signal } );
-
-          if (!res.ok)
-            throw new Error("Something went wrong with fetching movies");
-
-          const data = await res.json();
-          if (data.Response === "False") throw new Error("Movie not found");
-
-          setMovies(data.Search);
-          setError("");
-        } catch (err) {
-          if (err.name !== "AbortError") {
-            console.log(err.message);
-            setError(err.message);
-          }
-        } finally {
-          setIsLoading(false);
-        }
-      }
-
-      if (query.length < 3) {
-        setMovies([]);
+    async function fetchMovies() {
+      try {
+        setIsLoading(true);
         setError("");
-        return;
+
+        const res = await fetch(
+          `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+          { signal: controller.signal }
+        );
+
+        if (!res.ok)
+          throw new Error("Something went wrong with fetching movies");
+
+        const data = await res.json();
+        if (data.Response === "False") throw new Error("Movie not found");
+
+        setMovies(data.Search);
+      } catch (err) {
+        if (err.name !== "AbortError") {
+          console.log(err.message);
+          setError(err.message);
+        }
+      } finally {
+        setIsLoading(false);
       }
+    }
 
-      fetchMovies();
+    if (query.length < 3) {
+      setMovies([]);
+      setError("");
+      return;
+    }
 
-      return function () {
-        controller.abort();
-      };
-    },
-    [query]
-  );
+    fetchMovies();
+
+    return () => controller.abort();
+  }, [query]);
 
   return { movies, isLoading, error };
 }
+
